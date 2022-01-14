431a97c5d01660384848dbcc790c3bc73c1e919d
Samsson R <91111124+Samsson56@users.noreply.github.com>
2022-01-14 20:20:28 +0530

    Add files via upload

---
+++ sam.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head><title>www.loyola-ICAMcollege.edu</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body style="background-color:Linen;margin:0">
+
+<div style="background-color: Crimson" class="header">
+<img style="float:left" src="logo.jpg" width="300" height="300"/>
+<a href="#"><h1>LOYOLA-ICAM COLLEGE OF ENGINEERING AND TECHNOLOGY</h1><br><h2>ESTABILISHED 2010</h2></a>
+
+<div class="header-menu">
+<a href="#"><h2>Home</h2></a>
+<a href="#"><h2>Courses</h2></a>
+<a href="dept.html"><h2>Dept</h2></a>
+<li><a href="#"><h2>Contact us</h2></a>
+<a href="#"><h2>About</h2></a></li>
+</div>
+</div>
+
+<center>
+<img src="licet intro.png" alt="centered image" />
+</center></br>
+
+<img src="intro2.jpg" width="770" height="550"/>
+<img src="intro4.jpg" width="770" height="550"/>
+<img src="intro5.jpg" width="770" height="550"/>
+<img src="intro6.jpg" width="770" height="550"/>
+
+<div style="background-color:AntiqueWhite;margin:0">
+
+<h1 style="text-align:center;color:Crimson">Indroduction to Loyola-ICAM</h1>
+<p style= "font-size:30px;">
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <?php 
+echo "The <b>Loyola-ICAM College of Engineering and Technology (LICET)</b> is a<b> private Catholic engineering and technology school</b> run by the Society of Jesus in Chennai, India. It is approved by AICTE and affiliated with the Anna University, Chennai. <b>It is a Christian Minority college</b>, founded in 2010 by the Jesuits as part of the Chennai Jesuit Mission's efforts to empower the Dalits, the poor and the marginalized."
+?>
+</p>
+
+<h1 style="text-align:center;color:Crimson">History</h1>
+<p style= "font-size:30px;">&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;In April 2009, Loyola College, Chennai, founded in 1925, and the Jesuit<b>Institut Catholique d’Arts et Métiers</b>,founded in 1898 in Lille, France, entered on a joint venture to found an engineering college in Chennai which would prepare Indian students to work in European industries and European students to work in Indian industries. Pursuant of this agreement, the Jesuits of Madurai Province founded Loyola-ICAM engineering college at the Loyola College campus in 2010.<b>It was officially inaugurated by Kalaignar M. Karunanidhi on 27 October 2010</b>.Based on principles of the Jesuit Ratio Studiorum with its humanistic emphasis, the study program aims at comprehensive personal development and at the ability to work as a team, develop communication skills, appreciate cultures, and be sensitive to ethical and social issues.</p>
+
+<h1 style="text-align:center;color:Crimson">Academics</h1>
+<p style= "font-size:30px;">
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;The college <b>offers courses at undergraduate levels (Bachelors) in engineering and technology</b>, including Mechanical Engineering, Electrical and Electronics Engineering, Electronics and Communication Engineering, Computer Science Engineering and Information Technology.In 2015–16, the sanctioned intake was 60/course, but 120 for Mechanical Engineering.<b>One aspect of Loyola College's affiliation with the Catholic Institute of Arts & Business (ICAM) in Lille, France, is the Master of Engineering degree for which Loyola-ICAM students may study for two years in France, after finishing their Bachelor studies in India. Students at Lille, in turn, can spend their fifth year of studies for the Master of Engineering in India</b>.</p>
+
+<h1 style="text-align:center;color:Crimson">Facilities</h1>
+<p style= "font-size:30px;">
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;LICET library contains over 25,000 books and receives 42 technical journals, besides E-resources like IEEE, ASME, ASTM, Springer, and Elsevier. It has Wi-Fi facility and has a capacity to accommodate 445 students.<br><br>
+
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Mechatronics lab to train students in such areas as <b>hydraulics and pneumatics, microcontrollers and PID controllers, as well as simulating pressure, flow, and temperature</b>for process and automation industries. The lab has simulators, a robot and a <b> human machine interface (HMI)</b> to control, monitor and integrate factory automation processes.<br><br>
+
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<b>Centre for Intelligent Systems (CIS)</b> facilitates students do web and mobile-based projects. Workshops related to Android and iOS application development are conducted by experts from the industry. Currently CIS is used as the centre for developing an Android/iOS Mobile App for an international client in France.<br><br>
+
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Centre of Peace is a common prayer hall of 4000 sq. ft., open to students of all faiths to pray and spend time in meditation, with the intent of raising all the students</p>
+</div>
+
+<address style="text-align:center;padding:20px"><b>
+Loyola Campus<br>
+Nungambakkam<br>
+Chennai<br>
+Tamil Nadu 600034<br>
+Phone: 044 2817 8490<br><br>
+&copy 2021 LICET. All Rights Reserved.</b>
+
+</body>
+</html>
